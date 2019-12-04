255af9a6cdf0f2280e06204c5d579b03bdb78bb9
munene19 <munenexv@gmail.com>
2019-12-04 13:21:01 +0300

    Add new changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -25,9 +25,9 @@ export class QuoteComponent implements OnInit {
 
   constructor(){
     this.quote=[
-      new Quote(1,"A true friend is someone who is there for you when he’d rather be anywhere else.","Len wein",new Date(2019,5,14)), 
-      new Quote(2,"A grieving woman could sit alone on a jetty in the early morning. But not with a book in her hands.","Pia juul",new Date(2019,5,14)), 
-      new Quote(3,"A painting is worth a thousand confused art-gallery visitors.","Ljupka Cvetanova",new Date(2019,5,14)),      
+      new Quote(1,"A true friend is someone who is there for you when he’d rather be anywhere else.","Len wein","Kenn",new Date(2019,5,14)), 
+      new Quote(2,"A grieving woman could sit alone on a jetty in the early morning. But not with a book in her hands.","Pia juul","Mike",new Date(2019,5,14)), 
+      new Quote(3,"A painting is worth a thousand confused art-gallery visitors.","Ljupka Cvetanova","Stevo",new Date(2019,5,14)),      
     ];
     
   } 
